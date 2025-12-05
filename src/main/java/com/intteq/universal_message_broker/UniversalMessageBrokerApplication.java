dff3cfeffcfa1c7fc078c674616945c26ed22ea3
dilhanpathum <dilhan.jayasiri@gmail.com>
2025-12-05 09:38:17 +0530

    skelton project

---
+++ src/main/java/com/intteq/universal_message_broker/UniversalMessageBrokerApplication.java
@@ -0,0 +1,13 @@
+package com.intteq.universal_message_broker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversalMessageBrokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversalMessageBrokerApplication.class, args);
+	}
+
+}
